9a1fd3323f7ffc13b30ccec9f3114f05671ae9ce
premnath <premdevapp@gmail.com>
2020-12-24 22:12:27 +0530

    up

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,7 @@
 import * as actionTypes from "../actions/actionTypes";
 
+import { updateObject } from "../utility";
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   bacon: 0.7,
@@ -16,35 +18,31 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
-        },
+      const updatedIngredients = updateObject(state.ingredients, {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+      });
+
+      const updatedState = updateObject(state, {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-      };
+      });
+      return updatedState;
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
         },
         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-      };
+      });
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: action.ingredients,
         totalPrice: 0.99,
         error: false,
-      };
+      });
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true,
-      };
+      return updateObject(state, { error: true });
     default:
       return state;
   }
