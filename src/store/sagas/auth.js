acd5bda00cdaf668d727fc9cbfdadb60dbeb7fc1
premnath <premdevapp@gmail.com>
2020-12-27 13:57:38 +0530

    new

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -24,10 +24,10 @@ export function* authUserSaga(action) {
     returnSecureToken: true,
   };
   let url =
-    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY";
+    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA7b9qlVvGtAlFE1l9Kb-L63HfqleDb8_c";
   if (!action.isSignup) {
     url =
-      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY";
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyA7b9qlVvGtAlFE1l9Kb-L63HfqleDb8_c";
   }
   try {
     const response = yield axios.post(url, authData);
